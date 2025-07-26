917cdd7a4ea043fda2a42228c09b4c4aa8afb6b5
ULRICH ALLISTER ATIENZA <C1944-24@itmlyceumalabang.onmicrosoft.com>
2025-07-26 12:35:27 +0800

    changed customer ID anbd Middle names of last two VIP customers

--- Inherit_InterFace/Controllers/HomeController.cs
+++ Inherit_InterFace/Controllers/HomeController.cs
@@ -14,8 +14,8 @@ namespace Inherit_InterFace.Controllers
             customers.Add(new Customer { CustomerId = 2, FirstName = "Robertus", MiddleName = "B.", LastName = "Apollonarius", Birthday = new DateTime(1985, 5, 23) });
             customers.Add(new Customer { CustomerId = 3, FirstName = "Pleusicles ", MiddleName = "C.", LastName = "Vindex", Birthday = new DateTime(1992, 12, 3) });
             customers.Add(new VipCustomer { CustomerId = 6, FirstName = "Turbalio", MiddleName = "F.", LastName = "Pictor", Birthday = new DateTime(1980, 7, 7) });
-            customers.Add(new VipCustomer { CustomerId = 6, FirstName = "Petrus", MiddleName = "F.", LastName = "Sartorius", Birthday = new DateTime(1980, 7, 7) });
-            customers.Add(new VipCustomer { CustomerId = 6, FirstName = "Antipho", MiddleName = "F.", LastName = "Tyrannus", Birthday = new DateTime(1988, 1, 12) });
+            customers.Add(new VipCustomer { CustomerId = 7, FirstName = "Petrus", MiddleName = "E.", LastName = "Sartorius", Birthday = new DateTime(1980, 7, 7) });
+            customers.Add(new VipCustomer { CustomerId = 8, FirstName = "Antipho", MiddleName = "L.", LastName = "Tyrannus", Birthday = new DateTime(1988, 1, 12) });
 
             return View(customers);
 
