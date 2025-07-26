927f28a7b5b86adcd4e78e4a9fef44a01a849beb
ULRICH ALLISTER ATIENZA <C1944-24@itmlyceumalabang.onmicrosoft.com>
2025-07-26 12:32:22 +0800

    new vip customer

--- Inherit_InterFace/Controllers/HomeController.cs
+++ Inherit_InterFace/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace Inherit_InterFace.Controllers
             customers.Add(new Customer { CustomerId = 2, FirstName = "Robertus", MiddleName = "B.", LastName = "Apollonarius", Birthday = new DateTime(1985, 5, 23) });
             customers.Add(new Customer { CustomerId = 3, FirstName = "Pleusicles ", MiddleName = "C.", LastName = "Vindex", Birthday = new DateTime(1992, 12, 3) });
             customers.Add(new VipCustomer { CustomerId = 6, FirstName = "Turbalio", MiddleName = "F.", LastName = "Pictor", Birthday = new DateTime(1980, 7, 7) });
+            customers.Add(new VipCustomer { CustomerId = 6, FirstName = "Petrus", MiddleName = "F.", LastName = "Sartorius", Birthday = new DateTime(1980, 7, 7) });
 
             return View(customers);
 
